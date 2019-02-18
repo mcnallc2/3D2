08246e13ee3bb154652b8fb0faf902666a508717
Conor McNally <mcnallc2@ITSERV.SCSS.TCD.IE>
2019-02-18 19:21:25 +0000

    lastest update works on several tests

--- Practical2/App.s
+++ Practical2/App.s
@@ -7,100 +7,128 @@
 	EXPORT	start
 start
 
-	LDR R0, =INSTR
-	LDR R1, =LUT
-	MOV R2, #0
-	MOV R3, #0
-	MOV R4, #0
-	LDR R12, =0xA0000400
-	MOV R11, #10
-	CMP R0, #0
-	BPL pos
-	
-	;negating the value of R1 and changing the sign
-	NEG R0, R0
-	MOV R11, #11
-	
-pos	LDR R2, [R1]
+           LDR R0, =1049
+           LDR R1, =LUT
+           MOV R2, #0
+           MOV R3, #0
+           MOV R4, #0
+           MOV R5, #0
+           MOV R12, #0
+           MOV R11, #10
+           CMP R0, #0
+           BPL pos
+          
+           ;negating the value of R1 and changing the sign
+           NEG R0, R0
+           MOV R11, #11
+          
+pos    LDR R2, [R1], #4
 
 while
-	SUB R0, R0, R2
-	BMI next_check
-	ADD R4, #1
-	B while
-	
+           MOV R0, R0
+           SUBS R0, R0, R2
+           BMI next_check
+           ADD R4, #1
+           B while      
 next_check
-	ADD R0, R0, R2
-	STR R4, [R12], #4
-	CMP R2, #1
-	BEQ finish_check
-	LDR R2, [R1, #4]
-	B while
-	
+           ADD R0, R0, R2
+           MOV R4, R4
+           CMP R4, #0
+           BEQ skip_1
+           MOV R5, #1
+skip_1
+           MOV R5, R5
+           CMP R5 ,#0
+           BEQ skip_2
+           STR R4, [R13], #4
+		   ADD R12, R12, #-4
+skip_2
+           MOV R4, #0
+           MOV R2, R2
+           CMP R2, #1
+           BEQ finish_check
+           LDR R2, [R1], #4
+           B while     
+
 finish_check
 
-
-	;LDR R12, =0xA0000400
 	
-;IO1DIR	EQU	0xE0028018
-;IO1SET	EQU	0xE0028014
-;IO1CLR	EQU	0xE002801C
+IO1DIR	EQU	0xE0028018
+IO1SET	EQU	0xE0028014
+IO1CLR	EQU	0xE002801C
 
-	;ldr	r1,=IO1DIR
-	;ldr	r2,=0x000f0000	;select P1.19--P1.16
-	;str	r2,[r1]		;make them outputs
-	;ldr	r1,=IO1SET
-	;str	r2,[r1]		;set them to turn the LEDs off
-	;ldr	r2,=IO1CLR
+	ldr	r1,=IO1DIR
+	ldr	r2,=0x000f0000	;select P1.19--P1.16
+	str	r2,[r1]		;make them outputs
+	ldr	r1,=IO1SET
+	str	r2,[r1]		;set them to turn the LEDs off
+	ldr	r2,=IO1CLR
 	
-;; r1 points to the SET register
-;; r2 points to the CLEAR register
+; r1 points to the SET register
+; r2 points to the CLEAR register
 
-	;ldr	r3,=0x00010000	; start with P1.16.
-	;mov r8,r3, lsl #1  ;r10 is for P1.17
-	;mov r9,r3, lsl #2  ;r10 is for P1.18
-	;mov r10,r3, lsl #3  ;r10 is for P1.19
-		
-	;str	r3,[r1]	   	; clear the bit -> turn on the LED
-	;str r8,[r1]
-	;str r9,[r1]
-	;str r10,[r1]
-		
-;loop_
+	ldr	r3,=0x00010000	; start with P1.16.
+	mov r8,r3, lsl #1  ;r10 is for P1.17
+	mov r9,r3, lsl #2  ;r10 is for P1.18
+	mov r10,r3, lsl #3  ;r10 is for P1.19
 
-;;delay for about a second
-	;ldr	r4,=2000000
-;dloop	subs	r4,r4,#1
-	;bne	dloop
+loop_
+	MOV r7, r12
+	str	r3,[r1]	   	; clear the bit -> turn off the LEDs
+	str r8,[r1]
+	str r9,[r1]
+	str r10,[r1]
+
+;delay for about a second
+	ldr	r4,=40000000
+	mov r4, r4
+dloop	subs	r4,r4,#1
+	bne	dloop
 
 
-	;CMP r11, #10
-	;BNE nega
+	CMP r11, #10
+	BEQ positive
 	
-	;str	r3,[r2]	   	;turn on P1.16 LED (+) 
-	;str r8,[r1]		;turn off P1.17
-	;str r9,[r2]		;turn on P1.18
-	;str r10,[r1]	;turn off P1.19
+	str	r3,[r2]	   	;turn on P1.16 LED (+) 
+	str r8,[r1]		;turn off P1.17
+	str r9,[r2]		;turn on P1.18
+	str r10,[r2]	;turn off P1.19
+	B skip_pos
 	
-;nega
-	;str	r3,[r2]	   	;turn on P1.16 LED (+) 
-	;str r8,[r1]		;turn off P1.17
-	;str r9,[r2]		;turn on P1.18
-	;str r10,[r2]	;turn on P1.19
+positive
+	str	r3,[r2]	   	;turn on P1.16 LED (+) 
+	str r8,[r1]		;turn off P1.17
+	str r9,[r2]		;turn on P1.18
+	str r10,[r1]	;turn on P1.19
 	
-	;ldr	r4,=2000000 ;delay to show the sign
-;sign_delay	
-	;subs r4,r4,#1
-	;bne	sign_delay
+skip_pos
 
-	;ldr r5, [r12], #4
-	;BL check_num
+
+	ldr	r4,=20000000 ;delay to show the sign
+	mov r4, r4
+sign_delay	
+	subs r4,r4,#1
+	bne	sign_delay
+
+next_num
+	ldr r5, [r13, r7]
+	BL check_num
+	ADD r7, #4
 	
-	;ldr	r4,=2000000 ;delay to show the sign
-;num_delay	
-	;subs r4,r4,#1
-	;bne	num_delay
-	;B loop_
+	ldr	r4,=20000000 ;delay to show the sign
+	mov r4, r4
+num_delay	 
+	subs r4,r4,#1
+	bne	num_delay
+	
+	CMP r7, #0
+	BEQ last_num
+	B next_num
+	
+last_num
+	
+	
+	B loop_
 	
 INSTR DCD 1049
 LUT DCD 1000000000, 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1
@@ -109,25 +137,25 @@ stop	B	stop
 
 
 check_num
-	CMP r11, #9
+	CMP r5, #9
 	BEQ nine
-	CMP r11, #9
+	CMP r5, #8
 	BEQ eight
-	CMP r11, #9
+	CMP r5, #7
 	BEQ seven
-	CMP r11, #9
+	CMP r5, #6
 	BEQ six
-	CMP r11, #9
+	CMP r5, #5
 	BEQ five
-	CMP r11, #9
+	CMP r5, #4
 	BEQ four
-	CMP r11, #9
+	CMP r5, #3
 	BEQ three
-	CMP r11, #9
+	CMP r5, #2
 	BEQ two
-	CMP r11, #9
+	CMP r5, #1
 	BEQ one
-	CMP r11, #9
+	CMP r5, #0
 	BEQ zero
 
 nine 
