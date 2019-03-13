c71099757606854ce0241ef68321e9e9becbe9c3
Conor McNally <mcnallc2@itserv.scss.tcd.ie>
2019-03-13 11:49:06 +0000

    added commenting

--- Practical3/App.s
+++ Practical3/App.s
@@ -1,4 +1,4 @@
-AREA	AsmTemplate, CODE, READONLY
+	AREA	AsmTemplate, CODE, READONLY
 	IMPORT	main
 
 ; Practical 2 makes the 4 LEDs light up to represent a 32 bit number
@@ -42,57 +42,81 @@ IO1PIN  EQU 0xE0028010
 	;r5 now represents binary 1 on the leds
 	ldr r5, =0x00010000
 	
-			
+;---------------------------------------------------------------------------------
+
 ;entering main loop
 wloop
 	
 	;turning off all leds
 	str	r2,[r1]	
-	ldr r7,[r3]				
-	and r7, r7 ,#0x00f00000 ; masking buttons
+	;loading in the current value of the buttons
+	ldr r7,[r3]	
+	;masking the buttons as we are looking for a low signal for a pressed button
+	and r7, r7 ,#0x00F00000
 	
-	
+	;if P1.20 is pressed branch to increment
 	cmp r7, #0x00E00000
 	beq inc
+	;if P1.21 is pressed branch to decrement
 	cmp r7, #0x00D00000
 	beq dec
-	cmp r7, #0x00B00000 ; addition
+	;if P1.22 is pressed branch to addition
+	cmp r7, #0x00B00000
 	beq addi
-	cmp r7, #0x00700000 ; subtraction
+	;if P1.23 is pressed branch to subtraction
+	cmp r7, #0x00700000
 	beq subt
 	
+	;if nothing is pressed simply continue to loop
 	B wloop
 	
+;if inc call the increment subroutine and return to start
 inc
 	BL increment
 	B wloop	
+	
+;if dec call the decrement subroutineand and return to start
 dec
 	BL decrement
-	b wloop	
+	B wloop
+
+;if addi call the addition subroutineand and return to start
 addi
 	BL addition
 	B wloop	
+	
+;if subt call the subtraction subroutineand and return to start
 subt
 	BL subtraction
 	B wloop
 	
+;stop end of main loop; this can never be reached!!
 stop	B	stop
 	
+;-------------------------------------------------------------------
+
 ;subroutine for incrementing the current number
 increment
-		
-	ldr	r8,=1000000 ;delay for debounce
+	
+	;delay usede to debounce button
+	ldr	r8,=1000000
 	mov r8, r8
 _debounce	 
 	subs r8,r8,#1
 	bne	_debounce
 	
+	;load in value of button
 	ldr r7,[r3]
+	;mask the value
 	and r7, r7 ,#0x00F00000 
+	;if the button is still pressed its not bounce
 	cmp r7, #0x00E00000
+	;if there is bounce skip increment
 	bne finish1
 	
+	;add 1 to r4 (current number) 
 	add r4, r4, r5
+	;display current number on the leds
 	str r4,[r9]
 	
 	
@@ -104,23 +128,31 @@ _delay
 	bne	_delay
 	
 finish1
+	;link back to the main loop
 	BX	lr
 
+;---------------------------------------------------------------------
 
 ;subroutine for decrementing the current number
 decrement
 
-	ldr	r8,=1000000 ;delay for debounce
+	;delay usede to debounce button
+	ldr	r8,=1000000
 	mov r8, r8
 _debounce2	 
 	subs r8,r8,#1
 	bne	_debounce2
 	
+	;load in the current value of button
 	ldr r7,[r3]
-	and r7, r7 ,#0x00F00000 
+	;mask the value
+	and r7, r7 ,#0x00F00000
+	;if button is still pressed there was no bounce
 	cmp r7, #0x00D00000
+	;if there was bounce dont decrement
 	bne finish2
 	
+	;subtract 1 from the current number
 	sub r4, r4, r5
 	str r4,[r9]
 	
@@ -132,21 +164,28 @@ _delay2
 	bne	_delay2
 
 finish2
+	;branch back to main loop
 	BX	lr
+	
+;-----------------------------------------------------------------------
 
-
-;subroutine for addition
+;subroutine for addition and clearing last operation
 addition
 
-	ldr	r8,=1000000 ;delay for debounce
+	;delay used to debounce button
+	ldr	r8,=1000000
 	mov r8, r8
 _debounce3 
 	subs r8,r8,#1
 	bne	_debounce3
 	
+	;load current value of buttons
 	ldr r7,[r3]
+	;mask the buttons
 	and r7, r7 ,#0x00F00000 
+	;check if button is still pressed
 	cmp r7, #0x00B00000
+	;if ther was bounce do nothing and skip to end of subroutine
 	bne finish3
 	
 	;delay to check if there was a long press
@@ -158,13 +197,17 @@ _longpress
 	
 	;check value of buttons
 	ldr r7,[r3]
+	;mask buttons
 	and r7, r7 ,#0x00F00000 
 	;if button is still pressed it is a long press
 	cmp r7, #0x00B00000
 	;if its a long press skip over next steps
 	beq long_press
 	
+	;if it is not a long press we add
+	;add current number (r4) to previous stored value (r10) 
 	add r10, r10, r4
+	;display
 	str r10, [r9]
 	mov r12, r10
 	mov r4, #0
@@ -176,40 +219,51 @@ _delay3
 	subs r8,r8,#1
 	bne	_delay3
 	
+	;branch past the long press code
 	B finish3
-	
+
+;if it is a long press
 long_press
+	
+	;flash all leds to indicate long press
 	ldr r10, =0x000F0000
 	str r10, [r9]
 	
-	ldr	r8,=3000000 ;delay to show the number
+	;delay to flash the number for split second
+	ldr	r8,=3000000
 	mov r8, r8
 _delay3_1
 	subs r8,r8,#1
 	bne	_delay3_1
 
+	;delay to turn off leds for split second
 	str r10, [r1]
-	ldr	r8,=3000000 ;delay to show the number
+	ldr	r8,=3000000
 	mov r8, r8
 _delay3_2
 	subs r8,r8,#1
 	bne	_delay3_2
 	
+	;delay to flash leds again
 	str r10, [r9]
-	ldr	r8,=3000000 ;delay to show the number
+	ldr	r8,=3000000
 	mov r8, r8
 _delay3_3
 	subs r8,r8,#1
 	bne	_delay3_3
 	
+	;mov the previous -> previous stored value into r10 
 	mov r10, r12
+	;set current number (r4) to zero
 	mov r4, #0
 	
 finish3
+	;branch back to main loop
 	BX	lr
+
+;----------------------------------------------------------------------
 	
-	
-;subroutine for subtration and clearing everything
+;subroutine for subtration and clear all
 subtraction
 
 	;delay used for debounce
@@ -221,6 +275,7 @@ _debounce4
 	
 	;check value of the buttons
 	ldr r7,[r3]
+	;mask buttons
 	and r7, r7 ,#0x00F00000
 	;check if the button is still pressed 
 	cmp r7, #0x00700000
@@ -236,6 +291,7 @@ _longpress1
 	
 	;check value of buttons
 	ldr r7,[r3]
+	;mask buttons
 	and r7, r7 ,#0x00F00000 
 	;if button is still pressed it is a long press
 	cmp r7, #0x00700000
@@ -243,31 +299,40 @@ _longpress1
 	beq long_press1
 
 	;if it wasnt a long press 
-	;subtract current number from 
+	;subtract current number from previous stored value 
 	sub r10, r10, r4
 	;display the result of the previous calculation
 	str r10, [r9]
 	mov r4, #0
-	ldr	r8,=10000000 ;delay to show the number
+	
+	;delay to show the number for a split second
+	ldr	r8,=10000000
 	mov r8, r8
 _delay4_1
 	subs r8,r8,#1
 	bne	_delay4_1
 	
+	;branch past the long press code
 	B finish4
-	
+
+;if there was a long press
 long_press1
+
+	;flash all the leds once
 	ldr r10, =0x000F0000
 	str r10, [r9]
 	
-	ldr	r8,=10000000 ;delay to show the number
+	;delay to show the flash
+	ldr	r8,=10000000
 	mov r8, r8
 _delay4_2
 	subs r8,r8,#1
 	bne	_delay4_2
 	
+	;then reset all numbers to zero
 	mov r4, #0
 	mov r10,#0
+	mov r12,#0
 
 finish4
 	BX	lr
